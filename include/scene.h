4f20d91b9c1492ff477dc14dd831a427f786f896
steven <stevendixon1128@gmail.com>
2023-08-06 20:32:52 -0400

    Started adding images

---
+++ include/scene.h
@@ -0,0 +1,22 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+#include <citro2d.h>
+#include <3ds.h>
+
+class scene
+{
+private:
+    /* data */
+public:
+    scene(/* args */);
+    ~scene();
+};
+
+scene::scene(/* args */)
+{
+}
+
+scene::~scene()
+{
+}
